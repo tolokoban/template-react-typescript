307ec63ad6d635a94a6187cb713587c43be1d3dc
Tolokoban <contact@tolokoban.org>
2020-05-25 20:52:57 +0200

    Update App.tsx

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css"
+
+interface IAppProps {
+    className?: string
 }
+interface IAppState {}
 
-export default App;
+export default class App extends React.Component<IAppProps, IAppState> {
+    state = {}
+
+    render() {
+        const classes = ['App']
+        if (this.props.className) classes.push(this.props.className)
+
+        return (<div className={classes.join(' ')}>
+        </div>)
+    }
+}
