84320fb0b6e8196bc968f49236121687377bcf75
Tolokoban <contact@tolokoban.org>
2022-03-30 19:08:23 +0200

    Upgrade dependencies

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,23 +1,10 @@
 import React from "react"
-import Button from 'tfw/view/button'
-
 import "./App.css"
 
-interface IAppProps {
-    className?: string
-}
-interface IAppState {}
-
-export default class App extends React.Component<IAppProps, IAppState> {
-    state = {}
-
-    render() {
-        const classes = ['App']
-        if (this.props.className) classes.push(this.props.className)
-
-        return (<div className={classes.join(' ')}>
+export default function App() {
+    return (
+        <div className="App">
             <p>You are ready to play!</p>
-            <Button icon="play" />
-        </div>)
-    }
+        </div>
+    )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,51 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import Font from 'tfw/font'
-import Theme from 'tfw/theme'
+import App from "./App"
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
 
-import './index.css'
-
-
-async function start() {
-    // Set application theme.
-    await Font.loadJosefin(true)
-    Theme.apply({
-        color3: "#456",
-        color0: "#123",
-        colorPD: "#0040dd",
-        colorPL: "#56abff",
-        colorS: "#ff8d1e",
-        colorE: "#f44"
-    })
-
+function start() {
     // Create main component.
-    ReactDOM.render(
+    ReactDOM.createRoot(document.getElementById("ROOT")).render(
         <React.StrictMode>
             <App />
-        </React.StrictMode>,
-        document.getElementById('ROOT')
+        </React.StrictMode>
     )
+    // ReactDOM.render(
+    //     <React.StrictMode>
+    //         <App />
+    //     </React.StrictMode>,
+    //     document.getElementById("ROOT")
+    // )
 
     // Remove splash screen.
     removeSplashScreen()
 }
 
-
 function removeSplashScreen() {
-    const splash = document.getElementById('SPLASH')
+    const splash = document.getElementById("SPLASH")
     if (splash) {
         splash.classList.add("hide")
         const VANISHING_TIME = 1000
-        window.setTimeout(
-            () => {
-                const parent = splash.parentNode
-                if (parent) parent.removeChild(splash)
-            },
-            VANISHING_TIME
-        )
+        window.setTimeout(() => {
+            const parent = splash.parentNode
+            if (parent) parent.removeChild(splash)
+        }, VANISHING_TIME)
     }
 }
-
 
 start()
